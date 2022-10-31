c2d13499dfa47386ffbc2bb3740c24f36ec660c2
wtinker <will.c.tinker@gmail.com>
2022-10-30 18:08:14 -0700

    Add files via upload

---
+++ tournament/PoseStorage.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.drive.code.tournament;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+public class PoseStorage {
+    public static Pose2d transferpose = new Pose2d();
+}
